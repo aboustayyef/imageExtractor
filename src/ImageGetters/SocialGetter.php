90fa31a7563ab366743c6072b4957cfa9499b4e4
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2015-10-23 19:53:41 +0000

    less verbose

--- src/ImageGetters/SocialGetter.php
+++ src/ImageGetters/SocialGetter.php
@@ -21,7 +21,6 @@ class SocialGetter extends _Getter{
     		$min = (int) $this->minsize;
 
     		if ($width > $min) {
-                echo "Disqualified: $this->disqualified" . PHP_EOL;
                 if ($this->candidateImage != $this->disqualified) {
                     return $this->candidateImage;
                 }
